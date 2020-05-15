f7a43af3232c98c823c7ca8206eb5610046f3eae
Senatauro <cgsste@hotmail.com>
2020-05-15 16:30:31 -0300

    Parametro de animação para animos pré definidos
    Começando a adicionar os parametros para animações para animos pré definidos. Agora tem para o Animo concentrado, proximo passo, proximos animos

--- Assets/Scripts/Pessoa/Pessoa.cs
+++ Assets/Scripts/Pessoa/Pessoa.cs
@@ -13,15 +13,34 @@ public class Pessoa : MonoBehaviour
     {
         //transform.LookAt(GameObject.Find("Orador").transform, Vector3.up);
     }
+
+    /// <summary>
+    /// Função utilizada para determinar animação da pessoa a partir de um animo pré determinado. Olhar enum Animo
+    /// </summary>
+    /// <param name="comoEstou">Animo pré determinado</param>
     public void Animar(Animo comoEstou)
     {
-        meuAnimo = comoEstou;
-        animador.SetFloat("animacao", (float)comoEstou);
+        switch(comoEstou)
+        {
+            case Animo.CONCENTRADA:
+                animador.SetFloat("interesse", 1f);
+                animador.SetFloat("animacao", Random.Range(0f, 0.2f));
+                animador.SetFloat("inquietude", Random.Range(0f, 0.5f));
+                break;
+
+            case Animo.POUCO_CONCENTRADA:
+                animador.SetFloat("interesse", 1f);
+                animador.SetFloat("animacao", Random.Range(0f, 0.2f));
+                animador.SetFloat("inquietude", Random.Range(0f, 0.5f));
+                break;
+        }
     }
 
     public void FinalizarApresentação()
     {
-        
+        animador.SetFloat("animacao", 2f);
+        animador.SetFloat("inquietude", 0f);
+        animador.SetFloat("interesse", 1f);
     }
 
 }
